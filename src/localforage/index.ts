f1cf357190b60762c3ba4de2ed9253ea23596057
~朴：shu <1982392655@qq.com>
2025-05-23 10:22:35 +0800

    refactor(localforage): 使用 sessionStorage 替代 localStorage
    - 将 localforage 的存储机制从 localStorage 改为 sessionStorage
    - 删除了未使用的 mock 数据文件 sheets.json

--- src/localforage/index.ts
+++ src/localforage/index.ts
@@ -2,28 +2,30 @@
  * 使用 localstorage 实现本地存储
  */
 
-const com_prefix = 'es_co_';
+const com_prefix = "es_co_";
+
+const storage = window.sessionStorage;
 
 export const localForage = {
 	// 获取
 	getItem(key: string) {
-		const value = window.localStorage.getItem(com_prefix + key);
+		const value = storage.getItem(com_prefix + key);
 		if (value) return JSON.parse(value);
 		return null;
 	},
 
 	// 设置
 	setItem(key: string, value: unknown) {
-		window.localStorage.setItem(com_prefix + key, JSON.stringify(value));
+		storage.setItem(com_prefix + key, JSON.stringify(value));
 	},
 
 	// 移除
 	removeItem(key: string) {
-		window.localStorage.removeItem(com_prefix + key);
+		storage.removeItem(com_prefix + key);
 	},
 
 	// 清空
 	clear() {
-		window.localStorage.clear();
-	}
+		storage.clear();
+	},
 };
