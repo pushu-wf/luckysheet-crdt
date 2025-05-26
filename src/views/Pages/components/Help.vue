4cbe6cdc214371570e841025dd0a35096eb14821
~朴：shu <1982392655@qq.com>
2025-05-26 20:28:20 +0800

    refactor(components): 优化 Help 和 UserInfo 组件的结构
    - 在 Help 组件中添加了外层 div 容器，提高了组件的可维护性
    - 在 UserInfo 组件中：
      - 添加了外层 div 容器，使组件结构更加清晰
      - 调整了用户信息表单和按钮的位置，优化了布局
      - 重新组织了右侧功能项的结构，提高了可读性
      - 优化了密码修改和头像上传模态框的结构

--- src/views/Pages/components/Help.vue
+++ src/views/Pages/components/Help.vue
@@ -1,32 +1,34 @@
 <template>
-	<a-modal v-model:open="helpModalVisible" title="📣 帮助" style="width: 60%" cancelText="取消" okText="确认">
-		<div class="help-box">
-			<div class="help-btns">
-				<a-button style="margin-right: 10px">
-					<a href="https://dream-num.github.io/LuckysheetDocs/zh/guide/" target="_blank">
-						<ExportOutlined style="margin-right: 10px" />官方文档
-					</a>
-				</a-button>
-				<a-button style="margin-right: 10px">
-					<a href="https://blog.csdn.net/weixin_47746452/category_12491584.html?spm=1001.2014.3001.5482" target="_blank">
-						<ChromeOutlined style="margin-right: 10px" />相关博客
-					</a>
-				</a-button>
-				<a-button style="margin-right: 10px">
-					<a href="https://gitee.com/wfeng0/luckysheet-crdt/issues/new" target="_blank">
-						<GithubOutlined style="margin-right: 10px" />ISSUES
-					</a>
-				</a-button>
-				<a-button style="margin-right: 10px">
-					<a
-						href="https://www.bilibili.com/video/BV1TGk1YDEJj/?spm_id_from=333.1387.homepage.video_card.click&vd_source=604a25c77e296b4ce29b1e6e6cf03ea6"
-						target="_blank">
-						<YoutubeOutlined style="margin-right: 10px" /> Bilibili
-					</a>
-				</a-button>
+	<div>
+		<a-modal v-model:open="helpModalVisible" title="📣 帮助" style="width: 60%" cancelText="取消" okText="确认">
+			<div class="help-box">
+				<div class="help-btns">
+					<a-button style="margin-right: 10px">
+						<a href="https://dream-num.github.io/LuckysheetDocs/zh/guide/" target="_blank">
+							<ExportOutlined style="margin-right: 10px" />官方文档
+						</a>
+					</a-button>
+					<a-button style="margin-right: 10px">
+						<a href="https://blog.csdn.net/weixin_47746452/category_12491584.html?spm=1001.2014.3001.5482" target="_blank">
+							<ChromeOutlined style="margin-right: 10px" />相关博客
+						</a>
+					</a-button>
+					<a-button style="margin-right: 10px">
+						<a href="https://gitee.com/wfeng0/luckysheet-crdt/issues/new" target="_blank">
+							<GithubOutlined style="margin-right: 10px" />ISSUES
+						</a>
+					</a-button>
+					<a-button style="margin-right: 10px">
+						<a
+							href="https://www.bilibili.com/video/BV1TGk1YDEJj/?spm_id_from=333.1387.homepage.video_card.click&vd_source=604a25c77e296b4ce29b1e6e6cf03ea6"
+							target="_blank">
+							<YoutubeOutlined style="margin-right: 10px" /> Bilibili
+						</a>
+					</a-button>
+				</div>
 			</div>
-		</div>
-	</a-modal>
+		</a-modal>
+	</div>
 </template>
 
 <script lang="ts" setup>
