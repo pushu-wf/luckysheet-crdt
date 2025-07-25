4638730a54c4267c0723571166a369f13feb441f
~朴：shu <1982392655@qq.com>
2025-07-25 20:17:36 +0800

    refactor(server): 优化静态资源服务配置
    - 移除了多余的空格和换行符
    - 简化了 express.static 的调用方式

--- server/src/Middleware/index.ts
+++ server/src/Middleware/index.ts
@@ -8,9 +8,7 @@ function initStaticSource(app: express.Application) {
 	app.use(express.static(path.resolve(__dirname, "../../public")));
 	app.use(express.static(path.resolve(__dirname, "../../public/dist")));
 	app.use(express.static(path.resolve(__dirname, "../../public/uploads")));
-	app.use(
-		express.static(path.resolve(__dirname, "../../public/dist/assets"))
-	);
+	app.use(express.static(path.resolve(__dirname, "../../public/dist/assets")));
 }
 
 /**
