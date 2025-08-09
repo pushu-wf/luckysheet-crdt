1ab61868cec01211ef98260527c0f1a7e75e8c78
~朴：shu <1982392655@qq.com>
2025-08-09 10:09:05 +0800

    build: 重新构建master-vue 版本源码，修复已知BUG

--- public/lib/demoData/sheetCell.js
+++ public/lib/demoData/sheetCell.js
@@ -8,5 +8,6 @@ window.sheetCell = {
 		{ r: 0, c: 1, v: { v: "1", ct: { fa: "General", t: "n" }, m: "1" } },
 		{ r: 1, c: 0, v: { v: "B", ct: { fa: "General", t: "n" }, m: "1" } },
 		{ r: 1, c: 1, v: { v: "2", ct: { fa: "General", t: "n" }, m: "1" } },
+		{ r: 4, c: 6, v: { v: "4-6", ct: { fa: "General", t: "n" }, m: "1" } },
 	],
 };
